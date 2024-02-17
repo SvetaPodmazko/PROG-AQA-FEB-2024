7e777470059835f40660aed776627c244ada5256
SvitlanaPodmazko <podmazko00@icloud.com>
2024-02-17 20:05:02 +0200

    HomeWork(Session 3)

--- src/main/java/org/prog/cycles/CyclesDemo.java
+++ src/main/java/org/prog/cycles/CyclesDemo.java
@@ -1,62 +1,87 @@
 package org.prog.cycles;
 
+import java.util.Scanner;
+
 public class CyclesDemo {
 
-    public static void main(String[] args) {
-        boolean b1 = false;
-        boolean b2 = false;
-
-        if (trueFlag() || falseFlag() || b1) {
-            System.out.println("always true");
-        } else {
-            System.out.println("never works");
-        }
-
-        for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
-            if (i == 3) {
-                break;
-            }
-        }
-        int j = -10;
-        while (j > 0) {
-            System.out.println(j);
-            j--;
-        }
-
-        int k = -10;
-        do {
-            System.out.println(k);
-            k--;
-        } while (k >= 0);
-
-        test("CHROME");
-        test("Avant");
-    }
-
     //TODO: switch practice
-
-    private static void test(String s) {
-        switch (s) {
-            case "CHROME":
-                System.out.println("CHROME");
+    //Домашнее задания- Личный примертпрактик switch (Session 3)
+    public static void main(String[] args) {
+        Scanner scanner;
+        scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        switch (age){
+            case 16 :
+                System.out.println("Graduate from school");
                 break;
-            case "OPERA":
-                System.out.println("OPERA");
+            case 6:
+                System.out.println("You went to school");
                 break;
-            case "FIREFOX":
-                System.out.println("FIREFOX");
-                break;
-            case "EDGE":
-                System.out.println("EDGE");
-                break;
-            case "SAFARI":
-                System.out.println("SAFARI");
+            case 3:
+                System.out.println("You went to kindergarten");
                 break;
             default:
-                throw new RuntimeException("Unsupported browser!");
+                System.out.println("No matching condition found");
         }
     }
+
+
+
+//  public static void main(String[] args) {
+//        boolean b1 = false;
+//        boolean b2 = false;
+
+//        if (trueFlag() || falseFlag() || b1) {
+//            System.out.println("always true");
+//        } else {
+//            System.out.println("never works");
+//        }
+
+//        for (int i = 10; i >= 0; i--) {
+//            System.out.println(i);
+//            if (i == 3) {
+//                break;
+//            }
+//        }
+//        int j = -10;
+//        while (j > 0) {
+//            System.out.println(j);
+//            j--;
+//        }
+
+//        int k = -10;
+//        do {
+//            System.out.println(k);
+//            k--;
+//        } while (k >= 0);
+
+//        test("CHROME");
+//        test("Avant");
+//    }
+
+
+ //   private static void test(String s) {
+ //       switch (s) {
+ //           case "CHROME":
+ //               System.out.println("CHROME");
+ //               break;
+ //           case "OPERA":
+ //               System.out.println("OPERA");
+ //               break;
+ //                 case "FIREFOX":
+ //               System.out.println("FIREFOX");
+ //               break;
+ //           case "EDGE":
+ //               System.out.println("EDGE");
+ //               break;
+ //           case "SAFARI":
+ //               System.out.println("SAFARI");
+ //               break;
+ //           default:
+ //               throw new RuntimeException("Unsupported browser!");
+ //               }
+ //   }
 
     private static boolean falseFlag() {
         return false;
--- src/main/java/org/prog/exceptions/ExceptionsDemo.java
+++ src/main/java/org/prog/exceptions/ExceptionsDemo.java
@@ -10,38 +10,42 @@ import java.io.FileReader;
 //TODO: Convert FileNotFoundException into Throwable in readFile3
 //TODO: Process Throwable in readFile1
 public class ExceptionsDemo {
-
-    public static void main(String[] args) {
-        try {
-            readFile1();
-        } catch (Throwable t) {
-            System.out.println("OOPS");
+//Домашнее задание (Session 3)
+    public static void main(String[] args){
+        readFile1();
         }
-        System.out.println("test");
+
+    public static void readFile1(){
+        try {
+            readFile2();
+        }catch (Throwable e){
+            System.out.println("Caught Throwable with message: " + e. getMessage());
+        }
     }
 
-    public static void readFile1() {
-        readFile2();
-    }
-
-    public static void readFile2() {
+    public static void readFile2() throws Throwable {
         readFile3();
     }
 
-    public static void readFile3() {
-        readFile4();
+    public static void readFile3() throws Throwable {
+        try {
+            readFile4();
+        } catch (FileNotFoundException e){
+            throw new Throwable(e. getMessage());
+        }
+
     }
 
-    public static void readFile4() {
+    public static void readFile4() throws FileNotFoundException{
         try {
             readFile5();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (Throwable e) {
+            throw new FileNotFoundException("File not found");
         }
     }
 
     public static void readFile5() throws FileNotFoundException {
-        File f = new File("abc.txt");
+        File f = new File("e. getMessage");
         FileReader fileReader = new FileReader(f);
         f.setReadable(true);
     }
